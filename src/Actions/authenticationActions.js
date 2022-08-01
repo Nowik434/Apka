b51e816060d311ef49e06f31fc88ac049b02e628
VCCFUND <you@example.com>
2022-08-01 13:18:00 +0200

    First working version

--- src/Actions/authenticationActions.js
+++ src/Actions/authenticationActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const API_URL = "http://localhost:1337/api";
 
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const authHeader = () => {
     const user = JSON.parse(localStorage.getItem('user'));
